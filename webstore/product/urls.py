bc9214fe7fa6c377328f0f57537eee1d54e9f9e1
ganjeonghoon <gansf310@naver.com>
2022-03-04 14:14:13 +0900

    Edit URLConf, Model, View for detail page

---
+++ webstore/product/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from product import views
+from django.views.generic import TemplateView
+
+# http://127.0.0.1:8000/
+
+app_name = 'product'
+
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('product/<int:product_id>/', views.p_detail, name="p_detail"),
+]
