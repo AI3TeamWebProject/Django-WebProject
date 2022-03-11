522267151da2ee27356c783458b95cccaade4bd6
ganjeonghoon <gansf310@naver.com>
2022-03-11 13:13:32 +0900

    Add Review in Detail.html

--- webstore/product/views.py
+++ webstore/product/views.py
@@ -1,4 +1,4 @@
-from product.models import Product
+from product.models import Product, Review
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
 
@@ -12,8 +12,8 @@ from django.core.paginator import Paginator
 #     else:
 #         return redirect('home')
 
-def index(request):
 
+def index(request):
     products = Product.objects.all().order_by('-p_register_date')
     context = {
         'products': products
@@ -23,12 +23,12 @@ def index(request):
 
 def p_detail(request, product_id):
     product = Product.objects.get(pk=product_id)
+    reviews = Review.objects.filter(r_product=product)
     context = {
-        'product': product
+        'product': product,
+        'reviews': reviews
     }
     return render(request, 'product/detail.html', context)
-
-
 
 
 
